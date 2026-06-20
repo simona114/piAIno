42f641d4215d4766b63a8f0b474bff69b30fd0ba
Simona Nikolova <57250076+simona114@users.noreply.github.com>
2026-06-21 02:16:32 +0300

    Change default greeting text to "Hello, piAIno"
    Verified visually via an HTML mockup approved before implementation,
    compared against a device screenshot of the real running app
    (see ui-verification/hello/).

--- app/src/main/java/com/ssnlva/MainActivity.kt
+++ app/src/main/java/com/ssnlva/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PiAInoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "piAIno",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello, $name!",
         modifier = modifier
     )
 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PiAInoTheme {
-        Greeting("Android")
+        Greeting("piAIno")
     }
 }
