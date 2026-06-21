fb3f8f9098e08c14c85ceeb08cab8006f84c946a
Simona Nikolova <57250076+simona114@users.noreply.github.com>
2026-06-21 16:36:31 +0300

    Wire the piano screen into MainActivity

--- app/src/main/java/com/ssnlva/MainActivity.kt
+++ app/src/main/java/com/ssnlva/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.ssnlva.ui.piano.PianoScreen
 import com.ssnlva.ui.theme.PiAInoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PiAInoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "piAIno",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                PianoScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello, $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PiAInoTheme {
-        Greeting("piAIno")
     }
 }
